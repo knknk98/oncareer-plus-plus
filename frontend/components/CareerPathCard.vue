02e1fe7bcdf426f7222d116cc662ec5459461ee9
knknk98 <kayooshino@gmail.com>
2021-07-04 11:58:22 +0900

    カードコンポーネントにpropsを設定

--- frontend/components/CareerPathCard.vue
+++ frontend/components/CareerPathCard.vue
@@ -6,19 +6,19 @@
               <User />
             </div>
             <div class="attr_box d-flex flex-column align-self-center">
-              <div class="user_attr">社会人５〜１０年 | 男性</div>
-              <div class="industry">法人営業</div>
+              <div class="user_attr">{{userAttr}}</div>
+              <div class="industry">{{afterJobType}}</div>
             </div>
           </div>
           <v-simple-table class="job_type_table">
             <tbody>
             <tr>
-              <td class="job_type_title">退職時の職種</td>
-              <td >法人営業</td>
+              <td class="job_type_title">退社時の職種</td>
+              <td >{{beforeJobType}}</td>
             </tr>
             <tr>
               <td class="job_type_title">入社時の職種</td>
-              <td>法人営業</td>
+              <td>{{afterJobType}}</td>
             </tr>
             </tbody>
           </v-simple-table>
@@ -28,15 +28,15 @@
               <div class="d-flex">
                 <NoImage />
                 <div class="attr_box d-flex flex-column align-self-center">
-                  <div class="user_attr comp_name">リクルートスタッフィング</div>
-                  <div class="industry_arrow">法人営業</div>
+                  <div class="user_attr comp_name">{{beforeCompName}}</div>
+                  <div class="industry_arrow">{{beforeJobType}}</div>
                 </div>
               </div>
               <div class="d-flex">
                 <NoImage />
                 <div class="attr_box d-flex flex-column align-self-center ">
-                  <div class="user_attr comp_name">FUNDBOX</div>
-                  <div class="industry_arrow">法人営業</div>
+                  <div class="user_attr comp_name">{{afterCompName}}</div>
+                  <div class="industry_arrow">{{afterJobType}}</div>
                 </div>
               </div>
             </div>
@@ -44,29 +44,24 @@
           <section class="content_section">
             <h2 class="content_title">コンテンツ1</h2>
             <p>
-              4年目の初めに全社営業表彰を獲得したタイミングで、
-              将来のキャリアに不安を覚えて転職を志しました。グロービスに通学し、
-              このまま人材派遣営業を続けていては、キャリア選択の幅が狭くなる事を確信して転職活動を始めました
+              {{content1}}
             </p>
           </section>
           <section class="content_section">
             <h2 class="content_title">コンテンツ2</h2>
             <p>
-              法人営業であり、これまでの経験が活かせることに加えて、経営者に対しての提案、ITリテラシー、
-              金融リテラシーが身につく、大手ではなくこれから伸びていく会社、という事を重視しました
+              {{content2}}
             </p>
           </section>
           <section class="content_section">
             <h2 class="content_title">コンテンツ3</h2>
             <p>
-              フェイスブック
-              ・SNSでリクルーターから声がけがあり面談 ・当時はインスタグラムがまさに伸びている時期であり、ある程度将来性を感じていたが、
-              Webサービスなのでプラットフォーマーに一つ劣るところだけが気になっていた。 ・社長面接まで行なったが、別の候補者に内定が出たため最終選考で落選。
+              {{content3}}
             </p>
           </section>
           <hr class="my-3">
         </v-card-text>
-        <v-card-actions>
+        <v-card-actions v-if="isDetail">
           <v-spacer />
           <v-btn
             color="primary"
@@ -78,6 +73,55 @@
         </v-card-actions>
       </v-card>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+const DUMMY_CONTENTS1: String = "4年目の初めに全社営業表彰を獲得したタイミングで、将来のキャリアに不安を覚えて転職を志しました。グロービスに通学し、このまま人材派遣営業を続けていては、キャリア選択の幅が狭くなる事を確信して転職活動を始めました"
+const DUMMY_CONTENTS2: String = "法人営業であり、これまでの経験が活かせることに加えて、経営者に対しての提案、ITリテラシー、金融リテラシーが身につく、大手ではなくこれから伸びていく会社、という事を重視しました"
+const DUMMY_CONTENTS3: String = "フェイスブック：SNSでリクルーターから声がけがあり面談。当時はインスタグラムがまさに伸びている時期であり、ある程度将来性を感じていたが、Webサービスなのでプラットフォーマーに一つ劣るところだけが気になっていた。社長面接まで行なったが、別の候補者に内定が出たため最終選考で落選。"
+
+export default Vue.extend({
+  props: {
+    isDetail: {
+      type: Boolean,
+      default:true,
+    },
+    userAttr: {
+      type: String,
+      default: "社会人５〜１０年 | 男性",
+    },
+    beforeJobType: {
+      type: String,
+      default: "法人営業",
+    },
+    afterJobType: {
+      type: String,
+      default: "法人営業",
+    },
+    beforeCompName: {
+      type: String,
+      default: "リクルートスタッフィング",
+    },
+    afterCompName: {
+      type: String,
+      default: "FUNDBOOK",
+    },
+    content1: {
+      type: String,
+      default: DUMMY_CONTENTS1,
+    },
+    content2: {
+      type: String,
+      default: DUMMY_CONTENTS2,
+    },
+    content3: {
+      type: String,
+      default: DUMMY_CONTENTS3,
+    },
+
+  }
+})
+</script>
 
 <style>
 
@@ -128,9 +172,3 @@
   margin: 40px 0;
 }
 </style>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <CareerPathCard />
+      <CareerPathCard :is-detail=false />
     </v-col>
   </v-row>
 </template>
